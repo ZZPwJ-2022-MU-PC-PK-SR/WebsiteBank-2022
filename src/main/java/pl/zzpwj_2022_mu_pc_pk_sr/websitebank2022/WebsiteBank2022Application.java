403ab27ebddba0d1d808130383709d9176064122
Piotr Chłapiński <p.chlapinski@gmail.com>
2022-04-30 14:50:47 +0200

    Initial project

---
+++ src/main/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/WebsiteBank2022Application.java
@@ -0,0 +1,13 @@
+package pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBank2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteBank2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/WebsiteBank2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBank2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
