48429450e80193d9708b4e8650a5686578bba854
Piotr Chłapiński <p.chlapinski@gmail.com>
2022-05-08 16:48:08 +0200

    Added login module without GUI and cookies

---
+++ src/main/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/payload/response/MessageResponse.java
@@ -0,0 +1,2 @@
+package pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.payload.response;public class MessageResponse {
+}
---
+++ src/main/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/security/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,2 @@
+package pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.security.jwt;public class AuthEntryPointJwt {
+}
