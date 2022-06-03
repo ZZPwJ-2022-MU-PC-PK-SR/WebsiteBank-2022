e63895b1ee0f097bdb9bb45dea6424b619bb63b8
Vertix1o <piotrek.vladimir@gmail.com>
2022-06-04 01:01:09 +0200

    Fix Circle CI not passing, try #2

--- src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/AuthorizationCodeTest.java
+++ src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/AuthorizationCodeTest.java
@@ -4,14 +4,12 @@ package pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -35,7 +33,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Import({WebSecurityConfig.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ExtendWith(SpringExtension.class)
 public class AuthorizationCodeTest {
     @Autowired
     private MockMvc mockMvc;
@@ -44,6 +41,9 @@ public class AuthorizationCodeTest {
     @Autowired
     private AuthorizationCodeRepository authorizationCodeRepository;
 
+    String json;
+    ObjectMapper mapper = new ObjectMapper();
+
     @BeforeAll
     public void initialize() {
         userRepository.save(new User("usernamefine","user@yourdomain.com","password",
@@ -51,275 +51,69 @@ public class AuthorizationCodeTest {
                 "testaddress", "testcorrespondence"));
     }
 
-    @Nested
-    class generateCodesTestsSuite {
-        String json;
-        ObjectMapper mapper = new ObjectMapper();
-
-        @BeforeEach
-        public void generateInit() throws JsonProcessingException {
-            CodeGenerateRequest request = new CodeGenerateRequest();
-            request.setUsername("usernamefine");
-            json = new ObjectMapper().writeValueAsString(request);
-        }
-
-        @WithMockCustomAdmin
-        @Test
-        public void generateCodesAuthorizedTest() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
-                    contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE)).andExpect(
-                    result -> assertEquals(50, ((ArrayList<String>) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("codes")).size()))
-                    .andExpect(result-> assertEquals(200,result.getResponse().getStatus()));
-        }
-
-        @WithMockCustomAdmin
-        @Test
-        public void generateCodesWhenAlreadyGeneratedTest() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
-                    contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE));
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
-                    contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE)).andExpect(
-                    result -> assertEquals("User has active codes", mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("message")))
-                    .andExpect(result -> assertEquals(400,result.getResponse().getStatus()));
-        }
-
-
-        @WithMockCustomUser
-        @Test
-        public void generateCodesUnauthorizedUser() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
-                    contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE))
-                    .andExpect(result -> assertEquals("Unauthorized", (mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("error"))))
-                    .andExpect(result -> assertEquals(401,result.getResponse().getStatus()));
-        }
-
-        @Test
-        public void generateCodesUnauthorizedAnonymous() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
-                    contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE))
-                    .andExpect(result -> assertEquals("Unauthorized", (mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("error"))))
-                    .andExpect(result -> assertEquals(401,result.getResponse().getStatus()));
-        }
-
-        @WithMockCustomAdmin
-        @Test
-        public void generateCodesOnMissingUser() throws Exception {
-            CodeGenerateRequest request = new CodeGenerateRequest();
-            request.setUsername("missinguser");
-            json = new ObjectMapper().writeValueAsString(request);
-            try {
-                mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json)
-                        .contentType(MediaType.APPLICATION_JSON));
-            } catch(NestedServletException ex) {
-                assertInstanceOf(RuntimeException.class, ex.getCause());
-                assertEquals("No user with such username found",ex.getCause().getMessage());
-            }
-        }
+    @BeforeEach
+    public void generateInit() throws JsonProcessingException {
+        CodeGenerateRequest request = new CodeGenerateRequest();
+        request.setUsername("usernamefine");
+        json = new ObjectMapper().writeValueAsString(request);
     }
 
-    @Nested
-    class compromiseCodesTestSuite {
-        String json;
-        ObjectMapper mapper = new ObjectMapper();
 
-        @BeforeEach
-        public void compromiseInit() throws Exception {
-            CodeGenerateRequest request = new CodeGenerateRequest();
-            request.setUsername("usernamefine");
-            json = mapper.writeValueAsString(request);
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
-                    contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE));
-        }
-
-        @Test
-        @WithMockCustomAdmin
-        public void compromiseCodesAuthorizedTest() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/compromise_codes").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result -> assertEquals("Codes sucessfully compromised", mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("message")))
-                    .andExpect(result -> assertEquals(200, result.getResponse().getStatus()));
-        }
-
-        @Test
-        @WithMockCustomAdmin
-        public void compromiseCodesWhenNoCodesTest() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/compromise_codes").content(json)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE));
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/compromise_codes").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result -> assertEquals("User has no active codes", mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("message")))
-                    .andExpect(result -> assertEquals(400, result.getResponse().getStatus()));
-        }
-
-        @Test
-        @WithMockCustomUser
-        public void compromiseCodesWhenUnauthorizedUser() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/compromise_codes").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result -> assertEquals("Unauthorized", mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("error")))
-                    .andExpect(result -> assertEquals(401, result.getResponse().getStatus()));
-        }
-
-        @Test
-        @WithMockCustomUser
-        public void compromiseCodesWhenAnonymousUser() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/compromise_codes").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result -> assertEquals("Unauthorized", mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("error")))
-                    .andExpect(result -> assertEquals(401, result.getResponse().getStatus()));
-        }
-
-        @WithMockCustomAdmin
-        @Test
-        public void compromiseCodesOnMissingUser() throws Exception {
-            CodeGenerateRequest request = new CodeGenerateRequest();
-            request.setUsername("missinguser");
-            json = new ObjectMapper().writeValueAsString(request);
-            try {
-                mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/compromise_codes").content(json)
-                        .contentType(MediaType.APPLICATION_JSON));
-            } catch(NestedServletException ex) {
-                assertInstanceOf(RuntimeException.class, ex.getCause());
-                assertEquals("No user with such username found",ex.getCause().getMessage());
-            }
-        }
+    @WithMockCustomAdmin
+    @Test
+    public void generateCodesAuthorizedTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
+                contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL_VALUE)).andExpect(
+                result -> assertEquals(50, ((ArrayList<String>) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("codes")).size()))
+                .andExpect(result-> assertEquals(200,result.getResponse().getStatus()));
     }
 
-    @Nested
-    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-    class checkCodeTests {
-        CodeValidateRequest request;
-        AuthorizationCode firstCode;
-        AuthorizationCode secondCode;
-        AuthorizationCode lastCode;
-        ObjectMapper mapper = new ObjectMapper();
-        String json;
+    @WithMockCustomAdmin
+    @Test
+    public void generateCodesWhenAlreadyGeneratedTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
+                contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL_VALUE));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
+                contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL_VALUE)).andExpect(
+                result -> assertEquals("User has active codes", mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("message")))
+                .andExpect(result -> assertEquals(400,result.getResponse().getStatus()));
+    }
 
-        @BeforeAll
-        public void comrpomiseInitAll() throws Exception {
-            User user = userRepository.findByUsername("usernamefine").get();
 
-            firstCode = new AuthorizationCode(user,0);
-            secondCode = new AuthorizationCode(user,1);
-            lastCode = new AuthorizationCode(user,2);
+    @WithMockCustomUser
+    @Test
+    public void generateCodesUnauthorizedUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
+                contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL_VALUE))
+                .andExpect(result -> assertEquals("Unauthorized", (mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("error"))))
+                .andExpect(result -> assertEquals(401,result.getResponse().getStatus()));
+    }
 
-            authorizationCodeRepository.save(firstCode);
-            authorizationCodeRepository.save(secondCode);
-            authorizationCodeRepository.save(lastCode);
+    @Test
+    public void generateCodesUnauthorizedAnonymous() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json).
+                contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL_VALUE))
+                .andExpect(result -> assertEquals("Unauthorized", (mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("error"))))
+                .andExpect(result -> assertEquals(401,result.getResponse().getStatus()));
+    }
+
+    @WithMockCustomAdmin
+    @Test
+    public void generateCodesOnMissingUser() throws Exception {
+        CodeGenerateRequest request = new CodeGenerateRequest();
+        request.setUsername("missinguser");
+        json = new ObjectMapper().writeValueAsString(request);
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/generate_codes").content(json)
+                    .contentType(MediaType.APPLICATION_JSON));
+        } catch(NestedServletException ex) {
+            assertInstanceOf(RuntimeException.class, ex.getCause());
+            assertEquals("No user with such username found",ex.getCause().getMessage());
         }
-
-        @BeforeEach
-        public void compromiseInit() {
-            firstCode.setActive(true);
-            secondCode.setActive(true);
-            lastCode.setActive(true);
-
-            authorizationCodeRepository.save(firstCode);
-            authorizationCodeRepository.save(secondCode);
-            authorizationCodeRepository.save(lastCode);
-
-            request = new CodeValidateRequest();
-        }
-
-        @WithMockCustomUser
-        @Test
-        public void checkCodeFirstValidTest() throws Exception {
-            request.setCode(firstCode.getCode());
-            json = mapper.writeValueAsString(request);
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE))
-                    .andExpect(result-> assertTrue((Boolean) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("valid")));
-        }
-
-        @WithMockCustomUser
-        @Test
-        public void checkCodeSecondAndLastInvalidTest() throws Exception {
-            request.setCode(secondCode.getCode());
-            json = mapper.writeValueAsString(request);
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result-> assertFalse((Boolean) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("valid")));
-
-            request.setCode(lastCode.getCode());
-            json = mapper.writeValueAsString(request);
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result-> assertFalse((Boolean) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("valid")));
-        }
-
-        @WithMockCustomUser
-        @Test
-        public void checkCodeSecondValidAfterFirstUsed() throws Exception {
-            request.setCode(firstCode.getCode());
-            json = mapper.writeValueAsString(request);
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE));
-
-            request.setCode(secondCode.getCode());
-            json = mapper.writeValueAsString(request);
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result-> assertTrue((Boolean) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("valid")));
-
-        }
-
-        @WithMockCustomUser
-        @Test
-        public void checkCodeCannotUseSameCodeTwice() throws Exception {
-            request.setCode(firstCode.getCode());
-            json = mapper.writeValueAsString(request);
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaType.ALL_VALUE));
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result-> assertFalse((Boolean) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("valid")));
-
-        }
-
-        @WithMockCustomUser
-        @Test
-        public void checkCodeReturnFalseIfNoCodesPresent() throws Exception {
-            firstCode.setActive(false);
-            secondCode.setActive(false);
-            lastCode.setActive(false);
-
-            authorizationCodeRepository.save(firstCode);
-            authorizationCodeRepository.save(secondCode);
-            authorizationCodeRepository.save(lastCode);
-
-            request.setCode(firstCode.getCode());
-            json = mapper.writeValueAsString(request);
-
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/authcodes/validate_code").content(json)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .accept(MediaType.ALL_VALUE))
-                    .andExpect(result-> assertFalse((Boolean) mapper.readValue(result.getResponse().getContentAsString(), Map.class).get("valid")));
-        }
-
     }
 }
