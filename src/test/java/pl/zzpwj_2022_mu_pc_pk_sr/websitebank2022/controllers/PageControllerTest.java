9794a483ea9f5ace750789262ed4236a99112628
Vertix1o <piotrek.vladimir@gmail.com>
2022-06-07 15:16:37 +0200

    Revert "Merge branch 'main' into feature/transactions"
    This reverts commit 04149cb8d31b44cc89ecc7892bbf5c3abbebc1f1, reversing
    changes made to 30f8959372ac576b19fab48d3d930a5449b81b66.

--- src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/PageControllerTest.java
+++ src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/PageControllerTest.java
@@ -1,48 +1,24 @@
 package pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.controllers;
 
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
-import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.mockusers.WithMockCustomAdmin;
 import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.mockusers.WithMockCustomUser;
-import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.models.*;
-import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.repository.TransactionHistoryRepository;
 import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.repository.UserRepository;
 import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.security.WebSecurityConfig;
-import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.services.TransactionHistoryService;
-
-import javax.transaction.Transactional;
-import java.sql.SQLOutput;
-import java.time.LocalDate;
-import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureMockMvc
@@ -53,6 +29,8 @@ public class PageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+
 
 
     @Test
@@ -112,5 +90,9 @@ public class PageControllerTest {
     public void shouldAllowAccessToDashboardToAuthorizedAdminCheckMessage() throws Exception{
         mockMvc.perform(get("/api/logged/dashboard")).andExpect(status().isOk()).andExpect(result -> assertEquals(result.getResponse().getContentAsString(), "{\"message\":\"adminadmin\\n Imie : null\\n Nazwisko : null\\n Numer karty : null\\n\"}"));
     }
+
+
+
+
 
 }
