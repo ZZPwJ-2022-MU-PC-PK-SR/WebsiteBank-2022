9cb5865f57999dc74ab90d3ce4e64669cd4fa4e1
Piotr Kiełek <229910@edu.p.lodz.pl>
2022-06-07 23:01:03 +0200

    test CI #%

--- src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/THControllerTest.java
+++ src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/THControllerTest.java
@@ -54,7 +54,6 @@ public class THControllerTest {
         User testUser = new User("testuserSSS", "testuser@test.com","testpassword",
                 "testname","testsurname","012345678910","PPP123123",
                 "testaddress","testcorrespondence");
-        testUser.setId(1L);
         userRepository.save(testUser);
         BankAccount testBankAccount = new BankAccount();
         testBankAccount.setAccountNumber("12345678911111111111111111");
