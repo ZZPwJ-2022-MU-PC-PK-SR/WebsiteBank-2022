984b74c83e3be82fce54e5086010737840fd4420
maciek <maciekurbanski12@wp.pl>
2022-06-08 00:33:41 +0200

    final dashboard changes

--- src/main/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/repository/CardRepository.java
+++ src/main/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/repository/CardRepository.java
@@ -2,11 +2,14 @@ package pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.models.BankAccount;
 import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.models.Cards;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CardRepository extends JpaRepository<Cards, Long> {
     Optional<Cards> findById(Long id);
+//    List<Optional<Cards>> findByBank_account_id(String bank_account_id);
 }
