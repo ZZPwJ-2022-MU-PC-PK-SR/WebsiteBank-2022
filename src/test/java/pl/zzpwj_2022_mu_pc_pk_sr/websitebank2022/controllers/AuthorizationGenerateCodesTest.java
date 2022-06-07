249f43cfe6aef81cccdc563af2136abdb27c7834
Piotr Chłapiński <p.chlapinski@gmail.com>
2022-06-07 21:41:26 +0200

    Fix test v2

--- src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/AuthorizationGenerateCodesTest.java
+++ src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/AuthorizationGenerateCodesTest.java
@@ -43,6 +43,7 @@ public class AuthorizationGenerateCodesTest {
 
     @BeforeAll
     public void initialize() {
+        authorizationCodeRepository.deleteAll();
         userRepository.deleteAll();
         userRepository.save(new User("usernamefine","user@yourdomain.com","password",
                 "testname", "testsurname", "012345678910", "PPP123123",
