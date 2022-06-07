1e2395b83b03ae75d68a7753e86e4789a7e6e8eb
Piotr Kiełek <229910@edu.p.lodz.pl>
2022-06-07 22:53:42 +0200

    test CI #2

--- src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/THControllerTest.java
+++ src/test/java/pl/zzpwj_2022_mu_pc_pk_sr/websitebank2022/controllers/THControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cglib.core.CollectionUtils;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.mockusers.WithMockCustomUserTransaction;
 import pl.zzpwj_2022_mu_pc_pk_sr.websitebank2022.models.*;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@DirtiesContext(methodMode= DirtiesContext.MethodMode.BEFORE_METHOD)
 public class THControllerTest {
     @Autowired
     private MockMvc mockMvc;
